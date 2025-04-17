dfaa2cb08b12ae1e8c70be8f7c90e9129e3cece3
fendfinger <fendfinger@gmail.com>
2025-04-17 22:59:23 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+
+from flask import Flask, render_template_string
+from pytrends.request import TrendReq
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return '<h1>Hello, TLBC!</h1>'
+def get_daily_trends():
+    pytrends = TrendReq(hl='ko', tz=540)
+    df = pytrends.trending_searches(pn='south_korea')
+    return df[0].tolist()[:10]
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/")
+def home():
+    trends = get_daily_trends()
+    return render_template_string("""
+        <h1>📈 오늘의 인기 검색어 (대한민국)</h1>
+        <ul>
+        {% for trend in trends %}
+            <li>{{ trend }}</li>
+        {% endfor %}
+        </ul>
+    """, trends=trends)
+
+if __name__ == "__main__":
+    app.run(debug=True)
