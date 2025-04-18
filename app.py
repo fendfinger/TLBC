1ca3ac1cf71d2359c2e464c0b6331410d27062d7
fendfinger <fendfinger@gmail.com>
2025-04-18 15:05:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,7 @@ NAVER_CLIENT_SECRET = os.getenv("NAVER_CLIENT_SECRET")
 @app.route("/")
 def index():
     return f"네이버 클라이언트 ID: {NAVER_CLIENT_ID}"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render에서 자동으로 할당해줌
+    app.run(host='0.0.0.0', port=port)        # 외부에서 접속 가능하게 지정
